f41c8db07465f5578d004370899dd20db428b783
unknown <sizwenkala80@gmail.com>
2023-06-10 17:15:42 +0200

    adding adds

--- public/javascript/update.js
+++ public/javascript/update.js
@@ -1,16 +1,17 @@
-const deleteBlog= document.getElementById('delete_blog');
+const deleteBlog= document.querySelectorAll('.delete_blog').forEach((deleteBlog)=>{
+    deleteBlog.addEventListener('click', async(e)=>{
+        const objectId=e.target.parentElement.id;
+        alert(`you're about to DELETE the blog (id:${objectId})`)
+        fetch(`/remove_blog/${objectId}`,{
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data =>{
+            location.reload();
+        })
+        .catch(error =>{
+            console.error(error)
+        })
+    })
 
-deleteBlog.addEventListener('click', async(e)=>{
-    const objectId=e.target.parentElement.id;
-    console.log(objectId)
-    fetch(`/remove_blog/${objectId}`,{
-        method: 'DELETE'
-    })
-    .then(response => response.json())
-    .then(data =>{
-        location.reload();
-    })
-    .catch(error =>{
-        console.error(error)
-    })
 })
